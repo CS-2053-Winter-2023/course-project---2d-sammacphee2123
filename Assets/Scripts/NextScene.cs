1a6caf6719f7f2b672780a3f50ab239833d27090
JitaoYu6 <jyu6@unb.ca>
2023-04-02 19:26:06 -0300

    Story and Intro scene added

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -3,19 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class StartMenu : MonoBehaviour
+public class NextScene : MonoBehaviour
 {
-
-
-    public void StartGame()
+    public void goNext()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
-
 }
