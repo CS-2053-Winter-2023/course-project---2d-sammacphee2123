4a7f0a56a04f7c534a6f910d87f1daae3eb770b9
xlu6 <xlu6@unb.ca>
2023-04-16 16:04:43 -0300

    enemy ai added to main

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+   /* // Start is called before the first frame update
     private Animator anim;
     private bool run;
     public float speed;
@@ -65,5 +65,76 @@ public class EnemyController : MonoBehaviour
         else{
             directionX = -1f;
         }
+    }*/
+    public Transform player;
+    UnityEngine.AI.NavMeshAgent agent;
+    Rigidbody2D rb;
+    private float jumpSpeed = 11f;
+    private float directionX;
+    public float speed;
+    private float enemyToPlayerDistance;
+    private bool run;
+    private Animator anim;
+
+    void Start(){
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        run = false;
+    }
+
+    void Update(){
+        if (run)
+            anim.Play("Pink_Run");
+        else
+            anim.Play("Pink_Idle");
+
+        if (player != null)
+        {
+            EnemyDirection();
+            enemyToPlayerDistance = transform.position.x - player.position.x;
+            if((enemyToPlayerDistance >= 7) || (enemyToPlayerDistance <= -7)){
+                directionX = 1f;
+                rb.velocity = new Vector2(directionX * speed, rb.velocity.y);
+            }
+            //pathfinding
+            else{
+                agent.SetDestination(player.position);
+            }
+            run = true;
+        }
+        else
+            run = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "EnemySmallJump"){
+            //rb.AddForce(Vector2.up * 350f);
+            rb.velocity = new Vector2(1f, jumpSpeed);
+
+        }
+        else if (collision.gameObject.tag == "EnemyBigJump"){
+            rb.AddForce(Vector2.up * 500);
+        }
+        else if (collision.gameObject.CompareTag("knife"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    void EnemyDirection(){
+        Vector3 thescale = this.transform.localScale;
+        if(this.transform.position.x < this.player.position.x){
+            thescale.x = Mathf.Abs(thescale.x);
+            directionX = 1f;
+        }
+        else{
+            thescale.x = Mathf.Abs(thescale.x) * -1;
+            directionX = -1f;
+        }
+        this.transform.localScale = thescale;
     }
 }
